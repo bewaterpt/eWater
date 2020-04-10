f6a639945ba9c1ab8f9e6c4e482ee85b9528a90b
Bruno Martins ESC <bruno.f.martins@escoteiros.pt>
2020-04-10 05:35:07 +0100

    Change telescope gate config, in order for it to work with the app's permission model

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,7 +63,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return $user->roles()->pluck('name')->contains('Admin');
+            return $user->roles()->pluck('slug')->contains('admin');
         });
     }
 }
