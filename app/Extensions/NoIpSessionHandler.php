19ab103e5665d456b9df91906487360b6badec0d
Bruno Martins <bruno.martins@BEWATER.local>
2020-04-21 10:12:41 +0100

    Create and register the NOIp session handler

---
+++ app/Extensions/NoIpSessionHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Extensions;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\InteractsWithTime;
+use SessionHandlerInterface;
+
+class NoIpSessionHandler implements \SessionHandlerInterface
+{
+    /**
+     * Add the request information to the session payload.
+     *
+     * @param  array  $payload
+     * @return $this
+     */
+    protected function addRequestInformation(&$payload)
+    {
+        if ($this->container->bound('request')) {
+            $payload = array_merge($payload, [
+                'user_agent' => $this->userAgent(),
+            ]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get the IP address for the current request.
+     *
+     * @return string
+     */
+    protected function ipAddress()
+    {
+        return "";
+    }
+
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Auth;
+use View;
+use Session;
+use App\Models\Permission;
+use App\Extensions\NoIpSessionHandler;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +28,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // $is_logged_in = false;
+        // $user = Auth::user();
+        // $user_roles = null;
+
+        // if ($user) {
+        //     $user_roles = $user->roles;
+        // }
+
+
+        // $permission = new Permission();
+
+        Session::extend('noip', function ($app) {
+            // Return implementation of SessionHandlerInterface...
+            return new NoIpSessionHandler;
+        });
     }
 }
