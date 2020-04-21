d9c7d561f4500f2db91ed67029ca1b06261d978a
Bruno Martins <bruno.martins@BEWATER.local>
2020-04-21 09:53:03 +0100

    Add action buttons to user list table rows

--- resources/views/settings/users/index.blade.php
+++ resources/views/settings/users/index.blade.php
@@ -3,37 +3,71 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header">Users</div>
                 <div class="card-body">
-                    <table style="width: 100%">
-                        <tr>
-                            <th>
-                                Name
-                            </th>
-                            <th>
-                                Username
-                            </th>
-                            <th>
-                                Roles
-                            </th>
-                        </tr>
-                        @foreach( $users as $user)
+                    <table id="datatable-users" class="object-table" style="width: 100%">
+                        <thead>
                             <tr>
-                                <td>
-                                    {{$user['name']}}
-                                </td>
-                                <td>
-                                    {{$user['username']}}
-                                </td>
-                                <td>
-                                    @foreach($user['roles'] as $role)
-                                        {{$role->name}}
-                                    @endforeach
-                                </td>
+                                <th>
+                                    @Lang('general.name')
+                                </th>
+                                <th>
+                                    @Lang('general.username')
+                                </th>
+                                <th>
+                                    @Lang('general.roles')
+                                </th>
+                                <th>
+                                    @Lang('general.state')
+                                </th>
+                                <th>
+                                    @Lang('general.actions')
+                                </th>
                             </tr>
-                        @endforeach
+                        </thead>
+                        <tbody>
+                            @foreach( $users as $user)
+                                <tr>
+                                    <td>
+                                        <a href="{{Route('settings.users.view', ['id' => $user['id']])}}">
+                                            {{$user['name']}}
+                                        </a>
+                                    </td>
+                                    <td>
+                                        {{$user['username']}}
+                                    </td>
+                                    <td>
+                                        @foreach($user['roles'] as $role)
+                                            {{$role->name}}
+                                        @endforeach
+                                    </td>
+                                    <td style="text-align: center">
+                                        @if($user['enabled'])
+                                            <i class="fas fa-check" style="color: limegreen"></i>
+                                        @else
+                                            <i class="fas fa-times" style="color: red"></i>
+                                        @endif
+                                    </td>
+                                    <td class="actions">
+                                        <a class="" href="{{$user['id'] === Auth::user()->id ? Route('settings.users.edit_self') : Route('settings.users.edit', ['id' => $user['id']])}}" title="@Lang('general.action_edit')"><i class="fas fa-user-edit"></i></a>
+                                        @if($user['id'] !== Auth::user()->id)
+                                            <a href="{{Route('settings.users.toggle_state', ['id' => $user['id']])}}" class="{{ $user['enabled'] ? 'disable' : 'enable' }}" title="{{$user['enabled'] ? __('general.action_disable') : __('general.action_enable')}}">
+                                                @if($user['enabled'])
+                                                    <i class="fas fa-user-times"></i>
+                                                @else
+                                                    <i class="fas fa-user-check"></i>
+                                                @endif
+                                            </a>
+                                            {{-- @if($user['id'] !== 1)
+                                                <a href="{{Route('settings.users.delete', ['id' => $user['id']])}}" class="delete" title="@Lang('general.action_delete')"><i class="fas fa-trash-alt"></i></a>
+                                            @endif --}}
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>
