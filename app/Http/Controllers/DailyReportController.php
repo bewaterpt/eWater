071768bb038d8181b517c20105b85d6d2db21f1b
Bruno Martins <bruno.martins@BEWATER.local>
2020-08-12 11:09:12 +0100

    Testing byte to string conversion

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -122,7 +122,7 @@ class DailyReportController extends Controller
         } else {
             $currentUserRoles = Auth::user()->roles()->pluck('slug');
             $articles = Article::getDailyReportRelevantArticles()->pluck('cod', 'descricao')->map(function($element) {
-                return utf8_encode(utf8_decode($element));
+                return utf8_encode($element);
             });
             dd($articles);
             $workers = User::whereHas('roles', function ($query) use ($currentUserRoles){
