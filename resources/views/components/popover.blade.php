96782c266a3515dbd94ba3f42a69a10da4332b26
Bruno Martins <bruno.martins@BEWATER.local>
2020-09-10 16:43:55 +0100

    Move popover from partials to components

---
+++ resources/views/components/popover.blade.php
@@ -0,0 +1,12 @@
+<div id="{{ $id ?? "generic" }}-popover" class="popover d-none">
+    <div id="title">
+        <div class="{{ $type === 'error' ? 'bg-flamingo' : '' }}">
+            {!! $title ?? "Popover" !!}
+        </div>
+    </div>
+    <div id="content">
+        <div>
+            {!! $content ?? "Popover Generico " !!}
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/tooltip.blade.php
@@ -0,0 +1,7 @@
+<div id="{{ $id ?? 'generic' }}-tooltip" class="tooltip" role="tooltip">
+    <div class="arrow"></div><div class="tooltip-inner">
+        <div id="title">
+            {!! $content ?? "Tooltip Genérica" !!}
+        </div>
+    </div>
+</div>
