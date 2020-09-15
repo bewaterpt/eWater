bd3636380fdada9466d697056b2e365f46d0de1e
Bruno Martins <bruno.martins@BEWATER.local>
2020-09-15 18:30:17 +0100

    Update languages

--- resources/lang/pt/errors.php
+++ resources/lang/pt/errors.php
@@ -6,7 +6,7 @@ return [
     'work_not_exists' => 'O número de obra introduzido não é válido, por favor verifique se o introduziu corretamente.',
     'work_already_closed' => 'Esta obra encontra-se resolvida.',
     'work_canceled' => 'Esta obra encontra-se anulada.',
-    'invalid_work_number' => 'Um ou mais dos números de obra inseridos não é válido, por favor verifique os números de obra',
-    'wait_for_work_check' => 'Estamos a verificar a validez de um número de obra, por favor aguarde',
+    'invalid_work_number' => 'Um ou mais dos números de obra inseridos são inválidos, por favor verifique os números de obra',
+    'wait_for_work_check' => 'Estamos a verificar a validade de um número de obra, por favor aguarde',
     'unexpected_error' => 'Ocorreu um erro inesperado, tente novamente ou contacte o departamento de <a href="mailto:helpdesk@bewater.com.pt">Sistemas de Informação</a>.',
 ];
