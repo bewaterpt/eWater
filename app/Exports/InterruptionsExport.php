414dfc64c9ffd358d6a96b96294fa13cd34b780c
Bruno Martins <bruno.martins@BEWATER.local>
2020-10-29 20:03:39 +0000

    Testing with column types

--- app/Exports/InterruptionsExport.php
+++ app/Exports/InterruptionsExport.php
@@ -4,9 +4,11 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use App\Exports\Sheets\InterruptionsSheet;
 
-class InterruptionsExport implements WithMultipleSheets
+class InterruptionsExport implements WithMultipleSheets, WithColumnFormatting
 {
     use Exportable;
     /**
@@ -25,5 +27,37 @@ class InterruptionsExport implements WithMultipleSheets
         ];
 
         return $sheets;
+    }
+
+    public function columnFormats(): array
+    {
+        return [
+            'A' => NumberFormat::FORMAT_GENERAL,
+            'B' => NumberFormat::FORMAT_TEXT,
+            'C' => NumberFormat::FORMAT_TEXT,
+            'D' => NumberFormat::FORMAT_TEXT,
+            'E' => NumberFormat::FORMAT_TEXT,
+            'F' => NumberFormat::FORMAT_TEXT,
+            'G' => NumberFormat::FORMAT_TEXT,
+            'H' => NumberFormat::FORMAT_TEXT,
+            'I' => NumberFormat::FORMAT_GENERAL,
+            'J' => NumberFormat::FORMAT_GENERAL,
+            'K' => NumberFormat::FORMAT_GENERAL,
+            'L' => NumberFormat::FORMAT_GENERAL,
+            'M' => NumberFormat::FORMAT_GENERAL,
+            'N' => NumberFormat::FORMAT_GENERAL,
+            'O' => NumberFormat::FORMAT_GENERAL,
+            'P' => NumberFormat::FORMAT_GENERAL,
+            'Q' => NumberFormat::FORMAT_GENERAL,
+            'R' => NumberFormat::FORMAT_GENERAL,
+            'S' => NumberFormat::FORMAT_GENERAL,
+            'T' => NumberFormat::FORMAT_GENERAL,
+            'U' => NumberFormat::FORMAT_GENERAL,
+            'V' => NumberFormat::FORMAT_GENERAL,
+            'W' => NumberFormat::FORMAT_GENERAL,
+            'X' => NumberFormat::FORMAT_GENERAL,
+            'Y' => NumberFormat::FORMAT_GENERAL,
+            'Z' => NumberFormat::FORMAT_GENERAL,
+        ];
     }
 }
