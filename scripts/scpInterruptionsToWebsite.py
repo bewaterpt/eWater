41ca8646ee1defcde5d09f945ae07069a82df262
Bruno Martins <bruno.martins@bewgpt.com.pt>
2020-12-23 15:57:17 +0000

    Remove hardcoded server fingerprint and auto add key to known_hosts.

--- scripts/scpInterruptionsToWebsite.py
+++ scripts/scpInterruptionsToWebsite.py
@@ -1,9 +1,9 @@
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
-ssh.get_host_keys().add('ftp.bewater.com.pt', 'ssh-rsa', "SHA256:pEpWtb90dTZAofgJfg6qa9ZbRrn/ONe6jyr2wVWUxEU")
+ssh.set_missing_host_key_policy(AutoAddPolicy())
 ssh.connect(hostname='ftp.bewater.com.pt',
             port='999',
             username='ourem',
