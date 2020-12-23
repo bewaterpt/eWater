286cb4782cc308bf0cdebd168203b0baaf63f6cd
Bruno Martins <bruno.martins@bewgpt.com.pt>
2020-12-23 15:54:53 +0000

    Hardcode server fingerprint in python export script

--- scripts/scpInterruptionsToWebsite.py
+++ scripts/scpInterruptionsToWebsite.py
@@ -3,7 +3,7 @@ from scp import SCPClient
 
 ssh = SSHClient()
 ssh.load_system_host_keys()
-client.get_host_keys().add('ftp.bewater.com.pt', 'ssh-rsa', "SHA256:pEpWtb90dTZAofgJfg6qa9ZbRrn/ONe6jyr2wVWUxEU")
+ssh.get_host_keys().add('ftp.bewater.com.pt', 'ssh-rsa', "SHA256:pEpWtb90dTZAofgJfg6qa9ZbRrn/ONe6jyr2wVWUxEU")
 ssh.connect(hostname='ftp.bewater.com.pt',
             port='999',
             username='ourem',
